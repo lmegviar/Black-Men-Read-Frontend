aa88279bd3355c531853a51eacb174727d1904a3
Graham Wimbrow <wimbrow@protonmail.com>
2021-08-09 23:55:22 +0000

    cleaned up the component loader script. should be ready to update the props as defined by the backend API.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // open IIFE
 (function () {
+  /**
+   * a reflection
+   */
   function identity (a) {
     return a;
   }
@@ -11,19 +14,20 @@ issues have a cover image, title, writer, illustrator, colorist, inker, publishe
 
 we need the following:
 
-* a "comic book issue" display "component"
-* a factory that returns new comic book issue components
+- a "comic book issue" display "component"
+- a factory that returns new comic book issue components
 
 the comic book issue component may be rendered differently, and respond to different user inputs, when rendered at different viewport dimensions and/or on different browsers and devices.
 
 the factory wil be called by some other logic, in this case where the application fetches data and wants to display one or more comic book issues. it's up to the application to decide how and where to append those elements to the DOM.
 
 */
-
-var issueDetailsModel = [
+// the data model / types / transformations for a comic book issue
+var schema = [
   {
     name: "title",
-    type: String
+    type: String,
+    required: true
   },
   {
     name: "subtitle",
@@ -79,27 +83,12 @@ var issueDetailsModel = [
   }
 ];
 
-var issueRequired = [
-  "title",
-  "coverURL"
-];
-
 // set up a global namespace for the library
 var public = Object.create(null);
-// yes we could have collisions
+
 Object.defineProperty(window, "bmr", {
   value: public
 });
-
-/**
- * Iterates through the issueDetailsModel, basically forEach
- */
-function eachIssueModel (fn) {
-  for (var ndx = 0; ndx < issueDetailsModel.length; ndx++) {
-    // this should be made read-only
-    fn(issueDetailsModel[ndx]);
-  }
-}
 
 /**
  * Represents a comic book issue.
@@ -118,25 +107,32 @@ function eachIssueModel (fn) {
  * @param {string} details.thumbnailURL - the URL for a thumbnail image of the issue.
  * @param {string} details.coverURL - the URL for the issue's cover image.
  */
-function ComicBookIssue (details) {
+function Issue (details) {
   // define an empty object for the instance props
   var props = this.props = Object.create(null);
-  // reference the data model and assign instance props
-  eachIssueModel(function (model) {
-    if (
-      Object.prototype.hasOwnProperty.call(details, model.name) === false &&
-      issueRequired.indexOf(model.name) > -1
-    ) {
+  // assign instance props
+  for (var ndx = 0; ndx < schema.length; ndx++) {
+    // get a reference to the model
+    var model = schema[ndx];
+    // is this property in details
+    if (Object.prototype.hasOwnProperty.call(details, model.name)) {
+      // primitive type checking
+      if (details[model.name].constructor !== model.type) {
+        throw new TypeError("property " + model.name + " must be of type " + model.type.name);
+      }
+    } else if (model.required) {
+      // force these properties to be included
       throw new Error("property " + model.name + " is required");
     }
+    // set the property or default empty value for the type
     Object.defineProperty(props, model.name, {
       value: (model.transform || identity)(details[model.name] || new model.type())
     });
-  })
+  }
 }
 
 // define instance methods of comic book issues
-Object.defineProperties(ComicBookIssue.prototype, {
+Object.defineProperties(Issue.prototype, {
   render: {
     /**
      * returns a DOM node
@@ -148,21 +144,19 @@ Object.defineProperties(ComicBookIssue.prototype, {
       var template = "\
         <div class=\"issue\">\
           <header>\
-            <span class=\"title\">" + props.title + "</span>\
+            <span class=\"title\">" + props["title"] + "</span>\
           </header>\
         </div>\
       ";
-      // this could eventually be a custom element,
-      // with the template string handled as a real HTML <template>
-      return (function (el, html = "") {
-        // sanitize the html input
+      // wrap the retun in an IIFE so references to props can be forgotten
+      return (function (html = "") {
+        // create a DOM node, this could be a custom element in the future
+        const el = document.createElement("div");
+        // should sanitize the html string before we ask the DOM to parse it
         el.innerHTML = html;
         // return the element
         return el;
-      })(
-        document.createElement("div"),
-        template
-      );
+      })(template);
     }
   }
 });
@@ -170,7 +164,7 @@ Object.defineProperties(ComicBookIssue.prototype, {
 // "export" the factory as a library method named "create"
 Object.defineProperty(public, "create", {
   value: function (details) {
-    return new ComicBookIssue(details);
+    return new Issue(details);
   }
 });
 
